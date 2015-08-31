61b226edd8de949496db391ff7eb96bb65043f88
Gonzalo Peci <weedv2@outlook.com>
2015-09-01 01:54:16 +1200

    Remove redis.conf default from config, we will use the /etc/redis/redis.conf one. Merge valid run scripts to init.

--- init/11_config-redis.sh
+++ init/11_config-redis.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
+# If redi.conf file is missing copy one from the default
+if [ ! -f /config/redis.conf ]; then
+  cp /etc/redis/redis.conf /config/redis.conf
+  chown abc:abc /config/redis.conf && chmod 774 /config/redis.conf
+fi
+
+# Creat data dir and ensure ownership
 if [ ! -f /data ]; then
     mkdir /data
 fi
-chown abc -R /data 
+chown abc -R /data
 
-if [ -f /var/lib/redis ]; then
-    rm -rf /var/lib/redis && \
-    ln -s /data /var/lib/redis
-fi
-
+# Create config dir and ensure ownership
 if [ ! -f /config ]; then
     mkdir /config
 fi
 chown abc -R /config
 
+
 if [ -f /etc/redis ]; then
+    rm -rf /etc/redis.bkp && \
+    mv /etc/redis /etc/redis.bkp
     rm -rf /etc/redis && \
     ln -s /config /etc/redis
 fi
