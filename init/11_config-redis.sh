a3b0ea321394afb1341464f2df057a2b9ce56e3e
Gonzalo Peci <weedv2@outlook.com>
2015-09-05 10:14:20 +1200

    Optimize script by removing the if; then fi and remove /etc/redis work as its no longer needed

--- init/11_config-redis.sh
+++ init/11_config-redis.sh
@@ -7,21 +7,7 @@ if [ ! -f /config/redis.conf ]; then
 fi
 
 # Creat data dir and ensure ownership
-if [ ! -f /data ]; then
-    mkdir /data
-fi
-chown abc -R /data
+mkdir -p /data && chmod abc -R /data
 
 # Create config dir and ensure ownership
-if [ ! -f /config ]; then
-    mkdir /config
-fi
-chown abc -R /config
-
-
-if [ -f /etc/redis ]; then
-    rm -rf /etc/redis.bkp && \
-    mv /etc/redis /etc/redis.bkp
-    rm -rf /etc/redis && \
-    ln -s /config /etc/redis
-fi
+mkdir -p /config && chmod abc -R /config
