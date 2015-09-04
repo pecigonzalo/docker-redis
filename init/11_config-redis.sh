f996f9768f6a7bf6e7e25664e39dc221dfa6005d
Gonzalo Peci <weedv2@outlook.com>
2015-09-05 10:44:31 +1200

    sed should only run the first time we copy the default config file to avoid overwriting user settings.

--- init/11_config-redis.sh
+++ init/11_config-redis.sh
@@ -4,6 +4,9 @@
 if [ ! -f /config/redis.conf ]; then
   cp /etc/redis/redis.conf /config/redis.conf
   chown abc:abc /config/redis.conf && chmod 774 /config/redis.conf
+  sed 's/^daemonize yes/daemonize no/' -i /config/redis.conf
+  sed 's/^bind 127.0.0.1/bind 0.0.0.0/' -i /config/redis.conf
+  sed 's/^logfile \/var\/log\/redis\/redis-server.log/logfile \"\"/' -i /config/redis.conf
 fi
 
 # Creat data dir and ensure ownership
